a43000395f776d0b058410ff446107f69160e4ff
O-H-S <ohhyunsu0606@gmail.com>
2024-01-23 09:37:07 +0900

    테스트 유틸 클래스 리팩토링

--- src/test/java/com/ohs/monolithic/board/utils/BoardTestUtils.java
+++ src/test/java/com/ohs/monolithic/board/utils/BoardTestUtils.java
@@ -39,7 +39,7 @@ public class BoardTestUtils {
             .title(title)
             .description(desc)
             .build();
-    setEntityID(newBoard, "id", 1L);
+    setEntityID(newBoard, "id", id);
     return newBoard;
   }
 
@@ -70,6 +70,8 @@ public class BoardTestUtils {
     Post simplePost = Post.builder()
             .author(simpleAccount)
             .board(simepleBoard)
+            .title("test")
+            .content("test")
             .build();
     setEntityID(simplePost, "id", 1);
 
