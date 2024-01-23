534d2632e55053fc8c32aaff90ac41864a7711eb
O-H-S <ohhyunsu0606@gmail.com>
2024-01-23 09:35:38 +0900

    Account 클래스 리택토링 - 생성 목적에 따른 Builder 정의

--- src/main/java/com/ohs/monolithic/user/Account.java
+++ src/main/java/com/ohs/monolithic/user/Account.java
@@ -1,35 +1,39 @@
 package com.ohs.monolithic.user;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
+import org.apache.catalina.User;
 
 @Getter
 @Entity
-
-/*스프링 시큐리티에 이미 User 클래스가 있기 때문이다.
-물론 패키지명이 달라 User라는 이름을 사용할수 있지만 패키지 오용으로 인한 오류가 발생할수 있으므로 이 책에서는 User 대신 SiteUser라는 이름으로 명명하였다.*/
-
+@NoArgsConstructor
 public class Account {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(unique = true)
     private String username;
 
+    @Setter
     private String password;
 
+    @Setter
     @Column(unique = true, nullable = true)
     private String email;
 
+    @Setter
     @Column(nullable = true)
     private String provider;  // 소셜 로그인 제공자 (예: google, facebook, kakao)
 
+    @Setter
     @Column(nullable = true)
     private String providerId;  // 소셜 로그인 제공자에서의 사용자 ID
 
 
+    @Setter
     @Enumerated(EnumType.STRING)
     private UserRole role;
     @PostLoad
@@ -39,32 +43,23 @@ public class Account {
         }
     }
 
-
-    public void setProvider(String provider) {
-        this.provider = provider;
-    }
-
-    public void setProviderId(String providerId) {
-        this.providerId = providerId;
-    }
-
-
-    public void setUsername(String username) {
+    @Builder
+    public Account(String username, String password, String email, UserRole role){
         this.username = username;
-
-    }
-
-    public void setEmail(String email) {
+        this.password = password;
         this.email = email;
+        this.role = role;
 
     }
 
-    public void setPassword(String encode) {
-        this.password = encode;
-    }
-
-    public void setRole(UserRole role) {
+    @Builder(builderClassName = "WithProvider", builderMethodName = "WithProvider")
+    public Account(String provider, String providerId, UserRole role){
+        this.username = provider + providerId;
+        this.password = null;
+        this.provider = provider;
+        this.providerId = providerId;
         this.role = role;
     }
+
 
 }
