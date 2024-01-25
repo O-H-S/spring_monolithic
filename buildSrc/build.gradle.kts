1c51f40f0d1a518821e0cae5fa600da426021dc0
O-H-S <ohhyunsu0606@gmail.com>
2024-01-25 17:25:39 +0900

    -

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,21 @@
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+
+plugins {
+    //`kotlin-dsl`
+    //id("org.hidetake.ssh") version "2.11.2"
+
+
+}
+
+dependencies {
+    //implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    //api("org.hidetake:groovy-ssh:2.11.2")
+    //implementation("org.hidetake:groovy-ssh:2.11.2")
+    // Add other dependencies if needed
+}
+
