febeec8e7f75d5e738dae16398e51b90ef02429a
O-H-S <ohhyunsu0606@gmail.com>
2024-01-26 21:31:09 +0900

    프론트 코드 리팩토링

---
+++ src/main/resources/static/ohsite_utils.js
@@ -0,0 +1,28 @@
+var csrfToken = document.querySelector('meta[name="_csrf"]').getAttribute('content');
+var csrfHeader = document.querySelector('meta[name="_csrf_header"]').getAttribute('content');
+
+async function fetchWithCSRF(url, method, body = null) {
+    const headers = {
+        'Content-Type': 'application/json',
+        [csrfHeader]: csrfToken
+    };
+
+    let options = {
+        method: method,
+        headers: headers,
+        credentials: 'include'
+    };
+
+    if (body) {
+        options.body = JSON.stringify(body);
+    }
+
+
+    const response = await fetch(url, options);
+    /*if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(errorText);
+    }*/
+    return response;
+
+}
